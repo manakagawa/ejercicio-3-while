e910963bdf33978dac868fe1b7a9b9db3b71393a
manakagawa <40779515+manakagawa@users.noreply.github.com>
2024-07-10 19:22:24 -0600

    Add files via upload

---
+++ while.js
@@ -0,0 +1,14 @@
+let index = 11; 
+
+while(index <10){
+    console.log(index);
+    index++;
+}
+console.log("-----------")
+
+//do while, se ejecuta minimo una vez 
+let contador = 11;
+do {
+    contador++; // 0 + 1 = 1 
+    console.log(contador)
+} while(contador < 10)
